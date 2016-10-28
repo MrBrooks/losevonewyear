a9940456da711c5b10c28730028544234cd32a46
Denis Kulikov <altingfest@gmail.com>
2016-10-28 11:10:48 +0300

    alpha v

---
+++ src/js/vendor.js
@@ -0,0 +1,14 @@
+/**************************************/
+/* Vendor JavaScript files supervisor */
+/**************************************/
+
+/* Third party */
+/*
+//= ../../bower_components/jquery/dist/jquery.js
+/* //= ../../bower_components/bootstrap/dist/js/bootstrap.js */
+//= ../../bower_components/owl.carousel/owl.carousel.min.js
+/* //= ../../bower_components/mask-jquery/dist/jquery.mask.js */
+/* //= ../../bower_components/underscore/underscore-min.js */
+/* //= ../../bower_components/Snap.svg/dist/snap.svg-min.js */
+/* //= ../../bower_components/ajaxSubmit/ajaxSubmit.js */
+//= ../../bower_components/parallax/deploy/jquery.parallax.min.js
