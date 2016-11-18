f0d5e13c994b7e58105805e74062c7abc889399d
Denis Kulikov <altingfest@gmail.com>
2016-11-18 03:09:37 +0300

    add changes and png export

--- src/js/custom.js
+++ src/js/custom.js
@@ -22,20 +22,40 @@ $(document).ready(function() {
   CopyLink();
   var card_builder = new CardBuilder();
   var message_builder = new MessageBuilder();
-  var svg_card, svg_message;
+  var svg_card, svg_message, png_card, png_message;
+  var popup = new Popup(function(){
+    var sizes = losevo_video.getSizes();
+    popup.setSizes(sizes);
+    losevo_video.play();
+  }, function(){
+    losevo_video.stop();
+  });
 
   var tabs = new Tabs({
     btns: ".choose-send .pagination .item",
-    tabs: ".choose-send .tab-content"
+    tabs: ".choose-send .tab-content",
+    callbackOpen: function(el){
+      if($(window).width() < 768){
+        var index = $(el).attr('data-index');
+        $(".choose-send .pagination .item[data-index='"+index+"']").after($(".choose-send .tab-content[data-index='"+index+"']"));
+      }
+    }
   });
 
   var mobile_toy_tabs = new Tabs({
     effect: "active",
     btns: ".mobile-toy-tabs .item",
-    tabs: ".mobile-toy-tabs .tab-content"
+    tabs: ".mobile-toy-tabs .tab-content",
+    close: ".mobile-toy-tabs .close-tab",
+    callbackOpen: function(){
+      $(".card-start .logo").addClass('slideTop');
+    },
+    callbackClose: function(){
+      $(".card-start .logo").removeClass('slideTop');
+    }
   });
 
-  
+  var ajax_submit = new AjaxSubmit();
 
 
   var window_updater = new WindowUpdater([
@@ -168,7 +188,7 @@ function MessageBuilder(){
     //   width: size,
     //   height: size
     // });
-    fabric.loadSVGFromURL('img/svg/happy_new_year.svg', function(els,opts){
+    fabric.loadSVGFromURL(getPrefix()+'img/svg/happy_new_year.svg', function(els,opts){
       background = fabric.util.groupSVGElements(els,opts);
       background.width = 380;
       background.height = 380;
@@ -177,7 +197,25 @@ function MessageBuilder(){
     });
     
   }
-
+  this.toPNG = function(){
+    message_text = message.val();
+    if(text_box){
+      text_box.remove();
+    }
+    text_box = new fabric.Textbox(message_text, {
+      fontFamily: 'Bad Script',
+      fontSize: 14,
+      width: 212,
+      left: 89,
+      top:200
+    });
+    text_box.selectable  = false;
+    canvas.add(text_box);
+    var png = canvas.toDataURL({
+      format:'png'
+    });
+    return png;
+  };
   this.toSVG = function(){
     message_text = message.val();
     if(text_box){
@@ -207,7 +245,7 @@ function CardBuilder(){
   this.setTree = function(id){
     if(!tree || (tree_id !== id)){
       tree_id = id;
-      fabric.loadSVGFromURL("img/svg/tree-"+tree_id+".svg", function(els, opts){
+      fabric.loadSVGFromURL(getPrefix()+"img/svg/tree-"+tree_id+".svg", function(els, opts){
         if(tree){
           tree.remove();
         }
@@ -223,9 +261,35 @@ function CardBuilder(){
       });
     }
   };
+  this.toPNG = function(){
+    var png = canvas.toDataURL({
+      format:'png'
+    });
+    return png;
+  };
   this.toSVG = function(){
     return canvas.toSVG({
       viewBox:{x:0 , y:0, width: canvas_size, height: canvas_size}
+    },function(svg){
+      //fix to ImageMagic convert to png
+      // var op, fill, style;
+      // svg = svg.replace('stroke: none; stroke-width: 1; stroke-dasharray: none; stroke-linecap: butt; stroke-linejoin: miter; stroke-miterlimit: 10;','');
+
+      // if( (op = parseFloat($(svg).css('opacity'))) < 1){
+      //   fill = $(svg).css('fill');
+      //   fill = fill.replace('rgb','rgba').replace(')',','+op+')');
+      //   svg = $(svg).css('fill', fill).get(0).outerHTML;
+      // }
+      // style = $(svg).attr('style').split(';');
+      // style.pop();
+      // for(var i = 0; i < style.length; i++){
+      //   style[i] = style[i].replace(': ','="')+'"';
+      // }
+      // style = style.join(' ');
+      // svg = svg.replace('style',' '+style+' style');
+      // svg = $(svg).removeAttr('style').get(0).outerHTML;
+      // console.log(svg);
+      return svg;
     });
   };
   this.unsetBackground = function(){
@@ -237,7 +301,7 @@ function CardBuilder(){
     // console.log(canvas.getObjects());
   };
   this.setBackground = function(){
-    fabric.loadSVGFromURL('img/svg/back-full-'+background_index+'.svg',function(els,opts){
+    fabric.loadSVGFromURL(getPrefix()+'img/svg/back-full-'+background_index+'.svg',function(els,opts){
       if(background){
         canvas.remove(background);
       }
@@ -260,7 +324,6 @@ function CardBuilder(){
       canvas.remove(all[1]);        //all[] dynamicaly update after remove() operation
     }
   }
-
   function init(){
     elements = $(".pic[data-type]");
     background_btns = $("label[data-index]");
@@ -269,6 +332,7 @@ function CardBuilder(){
     remove_btn = $(".clear-all");
     message = card_builder.find(".message");
     message_close_btn = card_builder.find(".btn");
+    controls = $(".choose-toys .controls");
     canvas_size = $(window).width() >= 768? 380 : 320;
     if(document.getElementById('canvas')){
       canvas = new fabric.Canvas('canvas');
@@ -302,7 +366,7 @@ function CardBuilder(){
           break;
         default: throw Error("Unexpected element data-type in CardBuilder init()");
       }
-      var path = "img/svg/";
+      var path = getPrefix()+"img/svg/";
       fabric.loadSVGFromURL(path+type+"-"+index+".svg", function(els, opts){
         el = fabric.util.groupSVGElements(els,opts);
         el.set({
@@ -343,6 +407,7 @@ function CardBuilder(){
     });
     message_close_btn.on("click",function(){
       message.addClass("ok");
+      controls.addClass("active");
     });
   }
 
@@ -406,7 +471,13 @@ function Navigation(){
 
   function callback(id){
     switch(id){
+      case 'main-page':
+        scrollTop();
+        $("body").removeClass('card-start');
+        content_slider.trigger("to.owl.carousel",[0,400,true]);
+        break;
       case "card-start":
+        scrollTop();
         $("body").addClass(id);
         setTimeout(function(){
           content_slider.trigger("to.owl.carousel",[1,400,true]);
@@ -414,12 +485,14 @@ function Navigation(){
         
         break;
       case "card-tree":
+        scrollTop();
         $("body").removeClass('card-toys-page');
         content_slider.trigger("to.owl.carousel",[1,400,true]);
         steps_bar.to(1);
         break;
 
       case "card-toys":
+        scrollTop();
         $("body").addClass('card-toys-page');
         card_builder.unsetBackground();
         card_builder.setTree(parseInt($("#tree-slider .active [data-tree]").attr("data-tree")));
@@ -429,6 +502,7 @@ function Navigation(){
         break;
 
       case "card-message":
+        scrollTop();
         $("body").removeClass('card-toys-page');
         $("body").addClass('card-message-page');
         card_builder.setBackground();
@@ -439,9 +513,16 @@ function Navigation(){
         break;
 
       case "card-send":
+        scrollTop();
         $("body").removeClass('card-message-page');
+        png_card = card_builder.toPNG();
+        png_message = message_builder.toPNG();
+        $("#test").attr('src', png_card);
+        $("#test-m").attr('src', png_message);
+
         svg_message = message_builder.toSVG();
         svg_card = card_builder.toSVG();
+        submitCardAndMessage();
         $("#svg-card").html(svg_card);
         $("#svg-message").html(svg_message);
         content_slider.trigger("to.owl.carousel",[4,400,true]);
@@ -529,6 +610,42 @@ function WindowUpdater(opts){
   };
 
   self.onEvents();
+} 
+
+function submitCardAndMessage(){
+  var form = $("#card-data"),
+      card = $("#svg-card-data"),
+      message = $("#svg-message-data");
+  // var cut = svg_card.indexOf("<svg");
+  // svg_card = svg_card.slice(cut);
+  // cut = svg_message.indexOf("<svg");
+  // svg_message = svg_message.slice(cut);
+  // console.log(svg_card.length);
+  // var card64 = window.btoa(encodeURIComponent(svg_card));
+  // var message64 = window.btoa(encodeURIComponent(svg_message));
+  // console.log(card64.length);
+  // console.log(message64.length);
+  card.val(svg_card);
+  message.val(svg_message);
+
+  var data = form.serialize();
+  $.ajax({
+    url: getAjaxPrifix(),
+    type: 'POST',
+    data: data,
+    success: function(result) {
+      console.log(result);
+      data = JSON.parse(result);
+      $("#url-to-card").val(data.url);
+      $("[name='hash-id']").val(data.hash);
+      $("#download").attr('href',data.link);
+    },
+    error: function(one, two, three){
+      console.log(one);
+      console.log(two);
+      console.log(three);
+    }
+  });
 }
 
 });
@@ -537,32 +654,274 @@ function Tabs(options){
   var defs = {
     btns: ".tab-btn",
     tabs: ".tab-content",
-    effect: false
+    effect: false,
+    close: "",
+    callbackOpen: function(el){},
+    callbackClose: function(el){},
   };
   var opts = $.extend(defs, options);
 
-  var btns, tabs, group;
+  var btns, tabs, group, close;
 
   function init(){
     btns = $(opts.btns);
     tabs = $(opts.tabs);
-
+    if(opts.close){
+      close = $(opts.close);
+      close.on('click',closeTabs);
+    }
     btns.on('click',onClick);
   }
 
+  function closeTabs(){
+    tabs.removeClass('active');
+    btns.removeClass('active');
+    opts.callbackClose(this);
+  }
+
   function onClick(){
-      btns.removeClass('active');
-      if(opts.effect){
-        tabs.removeClass(opts.effect);
+    btns.removeClass('active');
+    if(opts.effect){
+      tabs.removeClass(opts.effect);
+    } else{
+      tabs.slideUp(300);
+    }
+    var index = parseInt($(this).addClass('active').attr('data-index'));
+    if(opts.effect){
+      $(tabs.get(index-1)).addClass(opts.effect);
+    } else{
+      $(tabs.get(index-1)).slideDown(300);
+    }
+    opts.callbackOpen(this);
+  }
+
+  init();
+}
+
+function getPrefix(){
+  return '';
+}
+
+function getAjaxPrifix(){
+  return '/losevonewyear/';
+}
+
+//require JQUERY
+function AjaxSubmit(options){
+  var def = {
+    form: '.ajax-submit',
+    btn: '[type="submit"]',
+    message: '.message',
+    isMessageInput: false,
+    inputs: 'input:not([type="submit"]), textarea',
+    invalid: 'invalid', //class
+    validate: validate, //boolean function(element)
+    message_succsess: "Отплавлено",
+    succsess_callback: function(){},
+    error_callback: function(){},
+  };
+  var opts = $.extend(def, options);
+
+  var form, btn, message, inputs, sendingFlag = false, message_init = "";
+
+  function init(){
+    form = $(opts.form);
+    btn = form.find(opts.btn);
+    message = form.find(opts.message);
+    // inputs = form.find(opts.inputs);
+    form.on("submit", submit);
+  }
+
+  function submit(){
+    var err = false;
+    inputs = $(this).find(opts.inputs);
+
+    inputs.each(function(){
+      if(opts.validate($(this))){
+        $(this).removeClass(opts.invalid);
       } else{
-        tabs.slideUp(300);
+        $(this).addClass(opts.invalid);
+        err = true;
       }
-      var index = parseInt($(this).addClass('active').attr('data-index'));
-      if(opts.effect){
-        $(tabs.get(index-1)).addClass(opts.effect);
+
+    });
+
+    if (!err){
+      var data = $(this).serialize();
+      $.ajax({
+         type: 'POST',
+         url: getAjaxPrifix(),
+         dataType: 'json',
+         data: data, 
+         beforeSend: beforeSend,
+         success: success,
+         error: error,
+         complete: complete
+      });
+    }
+    return false;
+  }
+
+  function beforeSend(){
+    if(opts.isMessageInput){
+      message_init = message.val();
+      message.val("Идет отправка...");
+    } else{
+      message_init = message.text();
+      message.text("Идет отправка...");
+    }
+    btn.prop('disabled', true);
+    sendingFlag = true;
+  }
+
+  function success(data){
+    sendingFlag = false;
+    if (data['error']) { 
+      if(opts.isMessageInput){
+        message.val("Ошибка");
       } else{
-        $(tabs.get(index-1)).slideDown(300);
+        message.text("Ошибка");
       }
+    } else {
+      if(opts.isMessageInput){
+        message.val(opts.message_succsess);
+      } else{
+        message.text(opts.message_succsess);
+      }
+    }
+    opts.succsess_callback();
+  }
+
+  function error(xhr, ajaxOptions, thrownError){
+    sendingFlag = false;
+    if(opts.isMessageInput){
+      message.val("Ошибка");
+    } else{
+      message.text("Ошибка");
+    }
+    opts.error_callback();
+    // console.log(xhr);
+    // console.log(ajaxOptions);
+    // console.log(thrownError);
+    // btn.prop('disabled', false);
+  }
+  function complete(){
+    if(sendingFlag){
+      message.text(message_init);
+      sendingFlag = false;
+    }
+    btn.prop('disabled', false);
+  }
+  function validate(el){
+    var field_type = el.attr('data-type');
+    switch(field_type){
+      case 'required': 
+        if (el.val() === ''){
+          return false;
+        }
+        break;
+      case 'email': 
+        var isemail = /.+@.+\..+/i;
+        var t = el.val();
+        if(t === '' || !isemail.test(t)){
+          return false;
+        }
+      break;
+      default: ;
+    }
+    
+    return true;
   }
   init();
+}
+
+function Popup(callback_open, callback_close){
+  var self = this;
+  var popup, btn_open, btn_close;
+
+  function init(){
+    btn_open = $(".popup-open-btn");
+    btn_close = $(".popup-close-btn");
+    popup = $(".popup");
+    content = popup.find(".popup-content");
+
+    btn_open.on('click', self.open);
+    btn_close.on('click', self.close);
+
+  }
+  this.open = function(){
+    if(callback_open){callback_open();}
+    popup.addClass('active');
+  };
+  this.close = function(){
+    popup.removeClass('active');
+    if(callback_close){callback_close();}
+  };
+  this.setSizes = function(s){
+    content.width(s.w);
+    content.height(s.h);
+  };
+  init();
+}
+
+function LoadYoutubeVideo(){
+  var height = window.innerHeight,
+      width = window.innerWidth,
+      ratio = 560/315,
+      padding = window.innerWidth > 767 ? 100 : 30;
+
+
+  function evalSize(){
+    var hp = height - padding*2,
+        wp = width - padding*2,
+        result = {};
+    var rp = wp/hp;
+    if(rp >= ratio){
+      result.h = hp;
+      result.w = hp*ratio;
+    } else{
+      result.w = wp;
+      result.h = wp/ratio;
+    }
+    return result;
+  }
+
+  var player, sizes;
+
+  function initPlayer() {
+    sizes = evalSize();
+    player = new YT.Player('player', {
+      height: sizes.h,
+      width: sizes.w,
+      videoId: 'OvW_L8sTu5E',
+      events: {
+        'onReady': onPlayerReady,
+      }
+    });
+  }
+  this.play = function(){
+    player.playVideo();
+  };
+  this.init = function(){
+    initPlayer();
+  };
+  this.stop = function(){
+    player.stopVideo();
+  };
+
+  this.getSizes = function(){
+    return sizes;
+  };
+
+  function onPlayerReady(){
+
+  };
+}
+var losevo_video  = new LoadYoutubeVideo();
+function onYouTubeIframeAPIReady(){
+  losevo_video.init();
+}
+
+function scrollTop(){
+  $('html,body').animate({scrollTop:0}, '500');
 }
